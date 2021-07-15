7c8b5128640ae729111296a144c2dd81d8d5d5b2
Bogdan Petkanich <bogdan.petkanich@nerdpeople.com>
2021-07-15 10:31:18 +0300

    Initial Commit

---
+++ ListSwiftUI/ListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListSwiftUIApp.swift
+//  ListSwiftUI
+//
+//  Created by echo on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
