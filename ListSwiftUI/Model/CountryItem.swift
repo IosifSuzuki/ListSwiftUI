b3576d99d0d838178a798ffeb327613c52bb3797
Bogdan Petkanich <bogdan.petkanich@nerdpeople.com>
2021-07-15 15:27:08 +0300

    add Country List screen

---
+++ ListSwiftUI/Model/CountryItem.swift
@@ -0,0 +1,13 @@
+//
+//  LocationItem.swift
+//  ListSwiftUI
+//
+//  Created by echo on 7/15/21.
+//
+
+import Foundation
+
+struct CountryItem: Identifiable {
+    let id = UUID()
+    let name: String
+}
---
+++ ListSwiftUI/ViewModel/ContentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentViewModel.swift
+//  ListSwiftUI
+//
+//  Created by echo on 7/15/21.
+//
+
+import Foundation
